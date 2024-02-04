f0c94df47a8d6afc51d2a70b3a8f3108d0f3d245
yuvaraj <yuvaa.it@gmail.com>
2024-02-04 22:52:52 +0100

    Implement navigate to login page when auth token does not exist, fetch members, technologies on load

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../Components/Header";
 import Teams from "../Components/Teams";
+import { withRouter } from "react-router";
 
 class Home extends Component {
   state = {
@@ -9,31 +10,66 @@ class Home extends Component {
     team: [], //holds the teams data got from backend
     edit: false, //handle edit mode for particular member 
     editId: undefined, //holds _id of member in edit mode
-    empId: "", 
+    empId: "",
     empName: "",
     experience: "",
     experienceFilter: "",
-    checked: "Expericence", 
+    checked: "Expericence",
   };
 
-   //onMounting:     
-      //if local storage had token proceed to set data in state
-      //if local storage does not have token route back to login page  
-      //code goes here to set initialData, team and data
-      //initialData - holds the data got from back-end
-      //data - holds data catagoried with team 
-      //team - holds teams data got from back-end
+  componentDidMount = async () => {
+    if (!this.getLocalStorage()) {
+      this.props.history && this.props.history.push('/login');
+      return;
+    }
+    const members = await this.handleGetMembers("/api/tracker/members/display");
+    const groupedMembers = members.reduce((acc, obj) => {
+      const key = obj.technology_name;
+      const curGroup = acc[key] ?? [];
+      return { ...acc, [key]: [...curGroup, obj] };
+    }, {});
+    const groupedMembersWithoutKey = Object.keys(groupedMembers).map((key) => groupedMembers[key]);
+    const techTeam = await this.handleGetTech();
+    this.setState({
+      initialData: members,
+      team: techTeam,
+      data: groupedMembersWithoutKey
+    })
+  }
+
+  //onMounting:     
+  //if local storage had token proceed to set data in state
+  //if local storage does not have token route back to login page  
+  //code goes here to set initialData, team and data
+  //initialData - holds the data got from back-end
+  //data - holds data catagoried with team 
+  //team - holds teams data got from back-end
 
   getLocalStorage = () => {
     //code goes here to get token value from local storage
+    return localStorage.getItem('authToken');
   };
 
   handleGetMembers = async (url) => {
     //code goes here to return the respose of getMember api
+    const response = await fetch(url, {
+      headers: {
+        Authorization:
+          `Bearer ${this.getLocalStorage()}`,
+      }
+    });
+    return response.json();
   };
 
   handleGetTech = async () => {
     //code goes here to return the respose of technologies get api
+    const response = await fetch('/api/tracker/technologies/get', {
+      headers: {
+        Authorization:
+          `Bearer ${this.getLocalStorage()}`,
+      }
+    });
+    return response.json();
   };
 
   handleChange = (e) => {
@@ -56,8 +92,8 @@ class Home extends Component {
     //code goes here for the onChange event of radio input fields
   };
 
-  handleClear=async()=>{
-   //code goes here to handle the clear button
+  handleClear = async () => {
+    //code goes here to handle the clear button
   }
 
   handleGo = async () => {
@@ -90,7 +126,8 @@ class Home extends Component {
           <button>Clear</button>
         </section>
         {/* display teams */}
-        <div className="noTeam">No Teams Found</div>
+        {this.state.team.length > 0 && <Teams team={this.state.team} />}
+        {this.state.team.length === 0 && <div className="noTeam">No Teams Found</div>}
         {/*Make sure, props name passed in child Component same as the state or function name which you are passing */}
       </>
     );
