a2849057bb772f152ed95676ba42e466694221f2
yuvaraj <yuvaa.it@gmail.com>
2024-02-04 21:39:22 +0100

    Implement Login page as instructed

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,30 +3,62 @@ import React, { Component } from "react";
 class Login extends Component {
   state = {
     name: "",
-    password: "",
+    password: ""
   };
 
-    //onMounting token stored in localStorage should be removed
+  //onMounting token stored in localStorage should be removed
+  componentDidMount(){
+    localStorage.removeItem("authToken")
+  }
 
   handleChange = (e) => {
     //code goes here to handle onChange event for input fields
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
   };
 
   loginRequest = async () => {
     //code goes here to return the response after hitting login api
+    const response = await fetch("/api/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        password: this.state.password
+      })
+    });
+    return response.json();
+    
   };
 
   handleLogin = async () => {
-     //handles login button
+    //handles login button
     //based on the login response, token should be set in local storage or alert should be displayed
+    const response = await this.loginRequest();
+    if (!response.token) {
+      alert("Invalid credentials!");
+      return;
+    }
+    localStorage.setItem("authToken", response.token);
+    this.props.history.push("/")
   };
+
+  isValidForm = () => {
+    return this.state.name.length > 0 && this.state.password.length > 0;
+  }
 
   render() {
     return (
       <div className="login">
         <h1>Login</h1>
         {/*code goes here to display input field to get name and password*/}
-        <button>
+        <input type="text" name="name" onChange={(e) => this.handleChange(e)} value={this.state.name} />
+        <input type="password" name="password" onChange={(e) => this.handleChange(e)} value={this.state.password} />
+        <button type="button" disabled={!this.isValidForm()} onClick={this.handleLogin}>
           Login
         </button>
       </div>
